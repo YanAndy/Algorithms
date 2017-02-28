c5dd743c0d43253d1e8bde8f300f081515937798
andy <2392276878@qq.com>
2017-02-28 13:26:12 -0800

    add test001

---
+++ src/Test001.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+/**
+ * Created by andy on 2/28/17.
+ */
+public class Test001 {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> pair=new HashMap<>();
+        int result[] = new int[2];
+        for(int i=0;i<nums.length;i++){
+            if(pair.containsKey(nums[i])){
+                result[0]=pair.get(nums[i]);
+                result[1]=i;
+                return result;
+            }
+            else pair.put(target-nums[i], i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args){
+        System.out.print("test001");
+    }
+}
