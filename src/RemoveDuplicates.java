c02a1327b538b24037d452640f22dd3ab0ad7607
andy <2392276878@qq.com>
2017-03-08 12:25:56 -0800

    add RemoveDuplicates II

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -24,12 +24,36 @@ public class RemoveDuplicates{
         }
         return res;
     }
+
+    /**
+     * Inplace modification
+     * @param array
+     */
+    public static void removeDuplicatesII(int[] array){
+        int k = 0;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < array.length; i++){
+            if(!set.contains(array[i])){
+                array[k++] = array[i];
+                set.add(array[i]);
+            }
+        }
+        for(int j = k; j < array.length; j++) array[j] = Integer.MIN_VALUE;
+    }
     public static void main(String[] args){
         //RemoveDuplicates r = new RemoveDuplicates();
         int[] array = new int[]{1,5,2,6,8,9,1,1,10,3,2,4,1,3,11,3};
         List<Integer> result = removeDuplicatesI(array);
         for(int n : result){
             System.out.print(n + ", ");
+        }
+        System.out.println();
+
+        removeDuplicatesII(array);
+
+        for(int i : array){
+            if(i == Integer.MIN_VALUE) break;
+            System.out.print(i + ", ");
         }
     }
 }
